bde3ed13286c23396f002bbb3f9b58c29fae5373
Sam Piper <sampii@protonmail.com>
2021-12-20 08:21:24 +0000

    feat: added user delete and view

--- src/resources/user_route.py
+++ src/resources/user_route.py
@@ -6,35 +6,83 @@ user_api = Blueprint('users', __name__)
 user_schema = user_schema()
 
 user_level_struct = {
-    0 : "Beginner",
-    5 : "Advanced",
+    0: "Beginner",
+    5: "Advanced",
     10: "Expert",
     50: "Insane"
 }
 
 
+@user_api.route('/view/<int:user_id>', methods=['GET'])
+def view_by_id(user_id):
+    """
+    Get a single user via their user ID
+    """
+    user = user_model.get_one_user(user_id)
+    if not user:
+        return custom_response({'error': 'user not found'}, 404)
 
-@user_api.route('/', methods=['GET','POST'])
+    ser_user = user_schema.dump(user)
+    return custom_response(ser_user, 200)
+
+
+@user_api.route('/view/<string:user_email>', methods=['GET'])
+def view_by_email(user_email):
+    """
+    Get a single user via their user email
+    """
+    user = user_model.get_user_by_email(user_email)
+    if not user:
+        return custom_response({'error': 'user not found'}, 404)
+
+    ser_user = user_schema.dump(user)
+    return custom_response(ser_user, 200)
+
+
+@user_api.route('/delete/<int:user_id>', methods=['DELETE'])
+def delete_by_id(user_id):
+    """
+    Delete a single user via their user id
+    """
+    user = user_model.get_one_user(user_id)
+    if not user:
+        return custom_response({'error': 'user not found'}, 404)
+    user.delete()
+    return custom_response({'message': 'deleted'}, 204)
+
+
+@user_api.route('/delete/<string:user_email>', methods=['DELETE'])
+def delete_by_email(user_email):
+    """
+    Delete a single user via their user email
+    """
+    user = user_model.get_user_by_email(user_email)
+    if not user:
+        return custom_response({'error': 'user not found'}, 404)
+    user.delete()
+    return custom_response({'message': 'deleted'}, 204)
+
+
+@user_api.route('/add', methods=['POST'])
 def create():
     """
     Create User Function
     """
     req_data = request.get_json()
-    score = req_data['social_credit_score']
-    level = ""
-    for key, value in user_level_struct.items():
-        if score >= key:
-            level = value
-    req_data['user_level'] = level
-    print(level)
+    print(req_data)
+
+    # Calculate the user level from score
+    req_data['user_level'] = calculate_level_from_score(
+        req_data['social_credit_score'])
 
     try:
         data = user_schema.load(req_data)
     except ValidationError as err:
-        print(err.messages)  # => {"email": ['"foo" is not a valid email address.']}
+        # => {"email": ['"foo" is not a valid email address.']}
+        print(err.messages)
         print(err.valid_data)  # => {"name": "John"}
         return custom_response(err.messages, 400)
-        
+
     # check if user already exist in the db
     user_in_db = user_model.get_user_by_email(data.get('email'))
     if user_in_db:
@@ -44,7 +92,8 @@ def create():
 
     user = user_model(data)
     user.save()
-    return custom_response("", 200)
+    return custom_response("Success", 200)
+
 
 def custom_response(res, status_code):
     """
@@ -55,3 +104,11 @@ def custom_response(res, status_code):
         response=json.dumps(res),
         status=status_code
     )
+
+
+def calculate_level_from_score(score):
+    level = ""
+    for key, value in user_level_struct.items():
+        if score >= key:
+            level = value
+    return level
