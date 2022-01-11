7b141be470f5a765974876b220f81347f708e7b7
Sam Piper <sampii@protonmail.com>
2022-01-11 11:48:48 +0000

    feat: implemented base for testing

---
+++ tests/common.py
@@ -0,0 +1,17 @@
+import unittest
+from dotenv import load_dotenv
+
+from src.app import create_app
+
+load_dotenv("../.env.test")
+
+class BaseCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.db = self.app.db
+
+    def tearDown(self):
+        # Delete Database collections after the test is complete
+        for collection in self.db.list_collection_names():
+            self.db.drop_collection(collection)
