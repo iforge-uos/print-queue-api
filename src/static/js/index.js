ceeec8e1449dfcb0f5bacaba3525e37404114f5e
Sam Piper <sampii@protonmail.com>
2022-02-23 21:11:17 +0000

    feat: changed stl viewer

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,5 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import HelloWorld from "./hello";
-  
-ReactDOM.render(<HelloWorld />, document.getElementById("react-root"));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {StlViewer} from "react-stl-viewer";
+
+const url = "https://storage.googleapis.com/ucloud-v3/ccab50f18fb14c91ccca300a.stl"
+
+const style = {
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+}
+
+function App() {
+    return (
+        <StlViewer
+            style={style}
+            orbitControls
+            shadows
+            url={url}
+        />
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('react-root'));
