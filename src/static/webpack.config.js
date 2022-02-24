57a6d65b5dad7cd5b01a62160386c157748a354f
Sam Piper <sampii@protonmail.com>
2022-02-24 18:19:28 +0000

    fix: added devtool source map for debugging

--- src/static/webpack.config.js
+++ src/static/webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const config = {
     entry:  __dirname + '/js/index.js',
+    devtool: 'eval-source-map',
     output: {
         path: __dirname + '/dist',
         filename: 'bundle.js',
