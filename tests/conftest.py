7019717718ba4932cf5930876b7fc49a6dcf505d
Sam Piper <sampii@protonmail.com>
2022-12-20 20:53:28 +0000

    feat: started db implementaiton for test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,61 @@
 import pytest
+import psycopg2
 from print_api.app import create_app
 
 
 @pytest.fixture()
 def app():
     app = create_app("testing")
-
+    init_database()
     yield app
 
     # clean up / reset resources here
 
 
+@pytest.fixture
+def init_database():
+    # Connect to the test database
+    conn = psycopg2.connector.connect(
+        # TODO replace with env vars
+        user="user",
+        password="password",
+        host="localhost",
+        database="testdb",
+    )
+    cursor = conn.cursor()
+
+    # Read the initialization queries from a file
+    # TODO fix mappings of file and write it
+    with open("init.sql", "r") as f:
+        queries = f.read()
+
+    # Execute the initialization queries
+    cursor.execute(queries)
+    conn.commit()
+
+    # Yield control back to the test function
+    yield
+
+    # Close the connection to the test database
+    cursor.close()
+    conn.close()
+
+
+def delete_database():
+    # TODO IMPLEMENT
+    pass
+
+
 @pytest.fixture()
 def client(app):
-    return app.test_client()
+    test_client = app.test_client()
+
+    def set_x_api_key(response):
+        response.headers["x-api-key"] = "test"
+        return response
+
+    test_client.application.after_request(set_x_api_key)
+    return test_client
 
 
 @pytest.fixture()
