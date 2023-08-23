a3ff4588ac70278d0bf2f551f7c4792303f3dd29
Sam Piper <sampii@protonmail.com>
2023-08-23 23:17:14 +0100

    fix: circular import issue

--- print_api/models/__init__.py
+++ print_api/models/__init__.py
@@ -5,9 +5,9 @@ db = SQLAlchemy()
 
 from .user_role import UserRole
 from .user import User, user_schema
+from .role_permission import RolePermission
 from .role import Role
 from .permission import Permission
-from .role_permission import RolePermission
 from .printers import Printer, printer_schema, printer_type, printer_location
 from .maintenance_logs import MaintenanceLog, maintenance_schema
 from .blacklisted_tokens import BlacklistedToken
